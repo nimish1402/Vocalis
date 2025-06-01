4941774621dc6da3234e6a2afa905c8f4961b7dc
nimish1402 <selotnimish1539@gmail.com>
2025-06-01 19:26:19 +0530

    Homepage setup complete

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import {ReactNode} from 'react'
 
 const RootLayout = ({children} : {children : ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href = "/" className = "flex items-center gap-2">
+            <Image src = "/logo.svg" alt = "Logo" width = {38} height = {32} />
+            <h2  className = "text-primary-100">Vocalis</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
   )
 }
 
